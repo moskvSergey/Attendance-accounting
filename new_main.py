f449c0bacb742003aa5797c3b47371ed0921e176
T Vr <reggg2004@mail.ru>
2024-06-12 18:22:55 +0700

    QQ

--- new_main.py
+++ new_main.py
@@ -1,5 +1,5 @@
 from datetime import datetime, timedelta
-from flask import Flask, render_template, redirect, request, abort, url_for
+from flask import Flask, render_template, redirect, session, request, abort, url_for
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 from data import db_session
 from data.groups import Groups
@@ -9,34 +9,12 @@ from data.lesson import Lesson
 from data.attendance import Attendance
 from data.forms import RegisterForm, LoginForm, PersonForm, GroupForm
 
-# import dlib
-# import cv2
-# import numpy as np
-# from ultralytics import YOLO
-
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'my_secret_key'
 login_manager = LoginManager()
 login_manager.init_app(app)
-
-
-# Инициализация моделей
-# model = YOLO('data/model.pt')
-# model.fuse()
-# detector = dlib.get_frontal_face_detector()
-# shape_predictor = dlib.shape_predictor('data/shape_predictor_68_face_landmarks.dat')
-# face_rec_model = dlib.face_recognition_model_v1('data/dlib_face_recognition_resnet_model_v1.dat')
-
-
-# def get_face_vector(frame):
-# gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-# faces = detector(gray)
-# for face in faces:
-# shape = shape_predictor(gray, face)
-# face_descriptor = face_rec_model.compute_face_descriptor(frame, shape)
-# return np.array(face_descriptor)
-# return None
+teac_id = None
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -49,6 +27,9 @@ def load_user(user_id):
 def logout():
     logout_user()
     return redirect("/")
+
+
+
 
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -74,33 +55,33 @@ def reqister():
         return redirect('/')
     return render_template('register.html', title='Регистрация', form=form)
 
-
 @app.route("/", methods=['GET', 'POST'])
 def start():
     return render_template('start.html')
-
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
         db_sess = db_session.create_session()
+        if 'user_id' in session:
+            user_id = session['user_id']
         user = db_sess.query(Teacher).filter(Teacher.login == form.login.data).first()
         if user and user.check_password(form.password.data):
             login_user(user, remember=form.remember_me.data)
-            return render_template('lesson.html', )
+            return render_template('lesson.html')
         return render_template('login.html',
                                message="Неправильный логин или пароль",
                                form=form)
     return render_template('login.html', title='Авторизация', form=form)
 
 
-@app.route('/lesson', methods=['GET', 'POST'])
-@login_required
-def lesson_check():
 
+@app.route('/lesson', methods=['GET', 'POST'])
+def lesson_check():
+    global teac_id
     db_sess = db_session.create_session()
-    lesson = db_sess.query(Lesson).filter(Lesson.teacher_id == id)
+    lesson = db_sess.query(Lesson).filter(Lesson.teacher_id == 1)
     # if current_user.is_authenticated:
     #     news = db_sess.query(News).filter(
     #         (News.user == current_user) | (News.is_private != True))
@@ -108,8 +89,6 @@ def lesson_check():
     #     news = db_sess.query(News).filter(News.is_private != True)
     return render_template("lesson.html", lesson=lesson)
 
-
-"""
 @app.route("/add_group", methods=['GET', 'POST'])
 def add_group():
     form = GroupForm()
@@ -120,41 +99,20 @@ def add_group():
         db_sess.add(group)
         db_sess.commit()
         return redirect(url_for("group_info", group_id=group.id))
-    return render_template('add_person.html', form=form)
-"""
-"""
-@app.route('/<user_id>', methods=['GET', 'POST'])
-@login_required
-def index(user_id):
+    return render_template('create_group.html', form=form)
+
+@app.route("/check_group", methods=['GET', 'POST'])
+def check_group():
     db_sess = db_session.create_session()
-    if current_user.is_authenticated:
-        current_time = datetime.now()
-        two_hours_ago = current_time - timedelta(hours=2)
-        lesson = db_sess.query(Lesson).filter(Lesson.teacher_id == user_id, Lesson.start_time < current_time,
-                                              Lesson.start_time > two_hours_ago).first()
-        if lesson:
-            group = db_sess.query(Groups).filter(Groups.id == lesson.group_id).first()
-            attendance_records = db_sess.query(Attendance, Person).join(Person).filter(
-                Attendance.lesson_id == lesson.id).all()
-            return render_template("index.html", grop_name=group.name, attendance_records=attendance_records)
-        else:
-            return redirect(url_for("add_group"))
-    else:
-        return redirect(url_for("login"))
+    group = db_sess.query(Groups)
+    # if current_user.is_authenticated:
+    #     news = db_sess.query(News).filter(
+    #         (News.user == current_user) | (News.is_private != True))
+    # else:
+    #     news = db_sess.query(News).filter(News.is_private != True)
+    return render_template("check_group.html", group=group)
 
-
-
-@app.route('/group/<group_id>', methods=['GET'])
-def group_info(group_id):
-    db_sess = db_session.create_session()
-    group = db_sess.query(Groups).filter(Groups.id == group_id).first()
-    if not group:
-        return "Группа не найдена", 404
-    return render_template('group_info.html', group=group)
-
-
-@app.route('/<group_id>/add_person', methods=['GET', 'POST'])
-@login_required
+@app.route('/check_group/<group_id>', methods=['GET', 'POST'])
 def add_jobs(group_id):
     form = PersonForm()
     if form.validate_on_submit():
@@ -171,7 +129,15 @@ def add_jobs(group_id):
         return redirect(url_for("group_info", id=group_id))
     return render_template('add_person.html', job='Добавление студента', form=form)
 
-"""
+
+
+@app.route('/group/<group_id>', methods=['GET'])
+def group_info(group_id):
+    db_sess = db_session.create_session()
+    group = db_sess.query(Groups).filter(Groups.id == group_id).first()
+    if not group:
+        return "Группа не найдена", 404
+    return render_template('group_info.html', group=group)
 if __name__ == "__main__":
     db_session.global_init("db/people.db")
     app.run(port=8080, host='127.0.0.1', debug=False)
